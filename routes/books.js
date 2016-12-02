1848b8df3f90dceb8d5514a6fcd6aa963929a578
Mason Embry <mason@embrycode.com>
2016-12-02 11:35:52 -0500

    findAll working for Book

--- routes/books.js
+++ routes/books.js
@@ -2,10 +2,13 @@
 
 var express = require('express');
 var router = express.Router();
+var Book = require('../models/index').Book;
 
 /* GET books page. */
 router.get('/', function(req, res, next) {
-  res.render('all_books', { title: 'Books' });
+  Book.findAll().then(function(books) {
+    res.render('all_books', { books: books, title: 'Books' });
+  });
 });
 
 module.exports = router;
