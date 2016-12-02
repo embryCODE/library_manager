ad047845adabe0718f6077bba8b0294753477938
Mason Embry <mason@embrycode.com>
2016-12-02 17:03:03 -0500

    Overdue and Checked Out working on Loans page

--- routes/loans.js
+++ routes/loans.js
@@ -6,9 +6,13 @@ var Loan = require('../models').Loan;
 
 /* GET loans page. */
 router.get('/', function(req, res, next) {
-  Loan.findAll({ include: [{ all: true }] }).then(function(loans) {
+  Loan.findAll({
+    include: [{
+      all: true
+    }]
+  }).then(function(results) {
     res.render('all_loans', {
-      loans: loans,
+      loans: results,
       title: 'Loans'
     });
   });
@@ -16,20 +20,35 @@ router.get('/', function(req, res, next) {
 
 /* GET checked out loans page. */
 router.get('/checked_out', function(req, res, next) {
-
-  res.render('checked_loans', {
-    title: "Checked Out Loans"
+  Loan.findAll({
+    where: {
+      returned_on: {
+        $not: null
+      }
+    }
+  }).then(function(results) {
+    res.render('checked_loans', {
+      loans: results,
+      title: "Checked Out Loans"
+    });
   });
-
 });
 
 /* GET overdue loans page. */
 router.get('/overdue', function(req, res, next) {
-
-  res.render('overdue_loans', {
-    title: "Overdue Loans"
+  Loan.findAll({
+    where: {
+      return_by: {
+        $lt: Date()
+      },
+      returned_on: null
+    }
+  }).then(function(results) {
+    res.render('overdue_loans', {
+      loans: results,
+      title: "Overdue Loans"
+    });
   });
-
 });
 
 module.exports = router;
