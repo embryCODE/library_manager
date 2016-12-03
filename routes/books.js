012b86a8ee89a4c54d43a6de7427749ce7208680
Mason Embry <mason@embrycode.com>
2016-12-03 10:55:13 -0500

    Filtering on Books works

--- routes/books.js
+++ routes/books.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var router = express.Router();
 var Book = require('../models').Book;
+var Loan = require('../models').Loan;
 
 /* GET books page. */
 router.get('/', function(req, res, next) {
@@ -16,7 +17,11 @@ router.get('/', function(req, res, next) {
 
 /* GET checked out books page. */
 router.get('/checked_out', function(req, res, next) {
-  Book.findAll().then(function(results) {
+  Book.findAll({
+    include: [
+      {model: Loan, where: {returned_on: null}}
+    ]
+  }).then(function(results) {
     res.render('checked_books', {
       books: results,
       title: "Checked Out Books"
@@ -26,7 +31,11 @@ router.get('/checked_out', function(req, res, next) {
 
 /* GET overdue books page. */
 router.get('/overdue', function(req, res, next) {
-  Book.findAll().then(function(results) {
+  Book.findAll({
+    include: [
+      {model: Loan, where: {return_by: {$lt: new Date()}}}
+    ]
+  }).then(function(results) {
     res.render('overdue_books', {
       books: results,
       title: "Overdue Books"
